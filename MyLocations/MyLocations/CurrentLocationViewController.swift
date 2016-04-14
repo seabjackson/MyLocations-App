42c4470989b831f43b98198e6ea62c1cea2e25d6
Seab <honeyboyseab@gmail.com>
2016-04-14 14:18:09 -0400

    Setting up the original UILabels and UIButtons

---
+++ MyLocations/MyLocations/CurrentLocationViewController.swift
@@ -0,0 +1,36 @@
+//
+//  FirstViewController.swift
+//  MyLocations
+//
+//  Created by lily on 4/14/16.
+//  Copyright © 2016 Seab Jackson. All rights reserved.
+//
+
+import UIKit
+
+class CurrentLocationViewController: UIViewController {
+    
+    @IBOutlet weak var messageLabel: UILabel!
+    @IBOutlet weak var latitudeLabel: UILabel!
+    @IBOutlet weak var longitudeLabel: UILabel!
+    @IBOutlet weak var addressLabel: UILabel!
+    @IBOutlet weak var tagButton: UIButton!
+    @IBOutlet weak var getButton: UIButton!
+    
+    @IBAction func getLocation() {
+        // do nothing
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ MyLocations/MyLocations/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  MyLocations
+//
+//  Created by lily on 4/14/16.
+//  Copyright © 2016 Seab Jackson. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
