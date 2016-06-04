18217ee5af798e1b67a5309ed69b44e40d81fa2c
Seab <honeyboyseab@gmail.com>
2016-06-04 00:15:18 -0400

    added an HUD View

--- MyLocations/MyLocations/HudView.swift
+++ MyLocations/MyLocations/HudView.swift
@@ -22,4 +22,20 @@ class HudView: UIView {
         
         return hudView
     }
+    
+    override func drawRect(rect: CGRect) {
+        let boxWidth: CGFloat = 96
+        let boxHeight: CGFloat = 96
+        
+        let boxRect = CGRect(x: round((bounds.size.width - boxWidth) / 2), y: round((bounds.size.height - boxHeight) / 2), width: boxWidth, height: boxHeight )
+        
+        let roundedRect = UIBezierPath(roundedRect: boxRect, cornerRadius: 10)
+        UIColor(white: 0.3, alpha: 0.8).setFill()
+        roundedRect.fill()
+        
+        if let image = UIImage(named: "Checkmark") {
+            let imagePoint = CGPoint(x: center.x - round(image.size.width / 2), y: center.y - round(image.size.height / 2) - boxHeight / 8)
+            image.drawAtPoint(imagePoint)
+        }
+    }
 }
